827f6510cf937977317d729d86c7b49af43951ed
Adrien Fournier <adrien.fournier1998@hotmail.fr>
2025-11-17 13:36:55 +0100

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,67 +6,87 @@
 /*   By: afournie <afournie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 15:45:01 by afournie          #+#    #+#             */
-/*   Updated: 2025/11/12 16:20:07 by afournie         ###   ########.fr       */
+/*   Updated: 2025/11/17 13:36:09 by afournie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include <stdio.h>
 
-int	ft_args_count(const char *s)
+int	ft_countargs(const char *s)
 {
-	int	args;
 	int	i;
+	int	ping;
 
 	i = 0;
-	args = 0;
+	ping = 0;
 	while (s[i])
 	{
 		if (s[i] == 37)
-			args++;
+			ping++;
 		i++;
 	}
-	return (args);
+	return (ping);
 }
 
-void	ft_print(const char *s, int index)
+int	ft_is_format(const char *s)
 {
-	while (s[index])
+	if (s == FORMAT_CHAR || s == FORMAT_DECIMAL || s == FORMAT_HEXALOW
+		|| s == FORMAT_HEXAUPP || s == FORMAT_INT || s == FORMAT_PERCENT
+		|| s == FORMAT_POINT || s == FORMAT_STR)
+		return (1);
+	else
+		return (0);
+}
+
+void	ft_print(const char *s, void *arg)
+{
+	if (s == FORMAT_CHAR)
 	{
-		if (s[index] != 37)
-			return ;
-		ft_putchar_fd(1, s[index]);
-		index++;
+	}
+	else if (s == FORMAT_DECIMAL)
+	{
+	}
+	else if (s == FORMAT_HEXALOW)
+	{
+	}
+	else if (s == FORMAT_HEXAUPP)
+	{
+	}
+	else if (s == FORMAT_INT)
+	{
+	}
+	else if (s == FORMAT_PERCENT)
+	{
+	}
+	else if (s == FORMAT_POINT)
+	{
+	}
+	else if (s == FORMAT_STR)
+	{
 	}
 }
 
 int	ft_printf(const char *s, ...)
 {
-	int		args_total;
-	int		args_count;
-	int		aa;
-	va_list	args_list;
-	va_list	args_copy;
+	int		i;
+	int		n;
+	int		args;
+	int		nombre_arguments;
+	va_list	liste_arguments;
 
-	aa = 0;
-	args_count = 0;
-	args_total = ft_args_count(s);
-	va_start(args_list, s);
-	va_copy(args_copy, args_list);
-	while (args_count < args_total)
+	i = 0;
+	n = 0;
+	nombre_arguments = ft_countargs(s);
+	va_start(liste_arguments, s);
+	while (s[i])
 	{
-		ft_print(s, aa);
-		if (aa)
-		{
-			printf("je stop ici");
-			return (1);
-		}
+		if (s[i] == "%" && ft_is_format(s[i + 1]))
+			ft_print(s, liste_arguments[n]) n++;
 	}
-	va_end(args_copy);
-	return (0);
 }
 
 int	main(void)
 {
-	ft_printf("Je fait un test avec % des lettres");
+	printf("Nombre : %d", ft_printf("igdhdg%ihi%fiiogd%idgdi"));
 }
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: afournie <afournie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 15:43:15 by afournie          #+#    #+#             */
-/*   Updated: 2025/11/12 15:44:50 by afournie         ###   ########.fr       */
+/*   Updated: 2025/11/17 13:35:57 by afournie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,19 @@
 
 # include "libft/libft.h"
 # include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define FORMAT_CHAR "c"
+# define FORMAT_STR "s"
+# define FORMAT_POINT "p"
+# define FORMAT_DECIMAL "d"
+# define FORMAT_INT "i"
+# define FORMAT_UDESC "u"
+# define FORMAT_HEXALOW "x"
+# define FORMAT_HEXAUPP "X"
+# define FORMAT_PERCENT "%"
+
+int	ft_printf(const char *s, ...);
 
 #endif
